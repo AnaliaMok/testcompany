410d5cc5ff9dd930efc896e26c24240bdc1c5eb3
Analia Mok <axm2580@g.rit.edu>
2018-05-19 21:58:37 -0400

    Added remaining cpt without extra acf settings. Added backup acf file

--- wordpress/wp-content/themes/testcompany/inc/custom_post_types.php
+++ wordpress/wp-content/themes/testcompany/inc/custom_post_types.php
@@ -93,3 +93,144 @@ function register_services_cpt() {
 }
 
 add_action( 'init', 'register_services_cpt', 0 );
+
+/**
+ * Post Type: Projects
+ */
+function register_projects_cpt() {
+	$labels = array(
+		"name" => __( "Projects", "" ),
+		"singular_name" => __( "Project", "" ),
+		"menu_name" => __( "Projects", "" ),
+		"all_items" => __( "All Projects", "" ),
+		"add_new" => __( "Add New", "" ),
+		"add_new_item" => __( "Add New Project", "" ),
+		"edit_item" => __( "Edit Project", "" ),
+		"new_item" => __( "New Project", "" ),
+		"view_item" => __( "View Project", "" ),
+		"view_items" => __( "View Projects", "" ),
+		"search_items" => __( "Search Projects", "" ),
+		"not_found" => __( "No Projects", "" ),
+		"not_found_in_trash" => __( "No Project found in trash", "" ),
+	);
+
+	$args = array(
+		"label" => __( "Projects", "" ),
+		"labels" => $labels,
+		"description" => "Client Projects",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "Projects",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "projects", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-art",
+		"supports" => array( "title", "thumbnail", "revisions" ),
+	);
+
+	register_post_type( "projects", $args );
+}
+
+add_action( 'init', 'register_projects_cpt', 0 );
+
+/**
+ * Post Type: Partners
+ */
+function register_partners_cpt() {
+	$labels = array(
+		"name" => __( "Partners", "" ),
+		"singular_name" => __( "Partner", "" ),
+		"menu_name" => __( "Partners", "" ),
+		"all_items" => __( "All Partners", "" ),
+		"add_new" => __( "Add New", "" ),
+		"add_new_item" => __( "Add New Partner", "" ),
+		"edit_item" => __( "Edit Partner", "" ),
+		"new_item" => __( "New Partner", "" ),
+		"view_item" => __( "View Partner", "" ),
+		"view_items" => __( "View Partners", "" ),
+		"search_items" => __( "Search Partners", "" ),
+		"not_found" => __( "No Partners", "" ),
+		"not_found_in_trash" => __( "No Partner found in trash", "" ),
+	);
+
+	$args = array(
+		"label" => __( "Partners", "" ),
+		"labels" => $labels,
+		"description" => "Client Partners",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "Partners",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "partners", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-universal-access",
+		"supports" => array( "title", "thumbnail", "revisions" ),
+	);
+
+	register_post_type( "partners", $args );
+}
+
+add_action( 'init', 'register_partners_cpt', 0 );
+
+/**
+ * Post Type: Employees
+ */
+function register_employees_cpt() {
+	$labels = array(
+		"name" => __( "Employees", "" ),
+		"singular_name" => __( "Employee", "" ),
+		"menu_name" => __( "Employees", "" ),
+		"all_items" => __( "All Employees", "" ),
+		"add_new" => __( "Add New", "" ),
+		"add_new_item" => __( "Add New Employee", "" ),
+		"edit_item" => __( "Edit Employee", "" ),
+		"new_item" => __( "New Employee", "" ),
+		"view_item" => __( "View Employee", "" ),
+		"view_items" => __( "View Employees", "" ),
+		"search_items" => __( "Search Employees", "" ),
+		"not_found" => __( "No Employees", "" ),
+		"not_found_in_trash" => __( "No Employee found in trash", "" ),
+	);
+
+	$args = array(
+		"label" => __( "Employees", "" ),
+		"labels" => $labels,
+		"description" => "Company Employees",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "employees",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "employees", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-groups",
+		"supports" => array( "title", "thumbnail", "revisions" ),
+	);
+
+	register_post_type( "employees", $args );
+}
+
+add_action( 'init', 'register_employees_cpt', 0 );
